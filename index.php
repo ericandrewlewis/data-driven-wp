f2de278f4db3c9b6b33b5516b702d4fcf4df372a
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-04-15 23:58:12 -0400

    On every admin page load.

--- index.php
+++ index.php
@@ -5,9 +5,6 @@ Version: 0.1
 */
 
 add_action( 'admin_enqueue_scripts', function( $hook_suffix ) {
-	if ( ! in_array( $hook_suffix, array( 'post.php', 'post-new.php' ) ) ) {
-		return;
-	}
 	wp_enqueue_script( 'ddwp', plugin_dir_url( __FILE__ ) . 'script.js', array( 'jquery' ) );
 	wp_localize_script( 'ddwp', 'ddwp',
 		array( 'uniqueHash' => md5( get_site_url() . get_current_user_id() ) )
