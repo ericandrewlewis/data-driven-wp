8d8cec211dac4b0a52b8a8df0ac592b57cfea221
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-04-15 23:57:22 -0400

    Log whenever a user hits an admin page.

--- index.php
+++ index.php
@@ -9,4 +9,7 @@ add_action( 'admin_enqueue_scripts', function( $hook_suffix ) {
 		return;
 	}
 	wp_enqueue_script( 'ddwp', plugin_dir_url( __FILE__ ) . 'script.js', array( 'jquery' ) );
+	wp_localize_script( 'ddwp', 'ddwp',
+		array( 'uniqueHash' => md5( get_site_url() . get_current_user_id() ) )
+	);
 } );
