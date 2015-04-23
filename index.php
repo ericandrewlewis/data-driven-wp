59a5d8b7b622a81e1aa9493d48e98f6f3d176294
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-04-22 21:07:25 -0400

    Move modernizr to its own file

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ Version: 0.1
 */
 
 add_action( 'admin_enqueue_scripts', function( $hook_suffix ) {
-	wp_enqueue_script( 'ddwp', plugin_dir_url( __FILE__ ) . 'script.js', array( 'jquery' ) );
+	wp_register_script( 'modernizr', plugin_dir_url( __FILE__ ) . 'modernizr.min.js' );
+	wp_enqueue_script( 'ddwp', plugin_dir_url( __FILE__ ) . 'script.js', array( 'modernizr', 'jquery' ) );
 	wp_localize_script( 'ddwp', 'ddwp',
 		array( 'uniqueHash' => md5( get_site_url() . get_current_user_id() ) )
 	);
