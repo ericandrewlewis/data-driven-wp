d7efb96f4b6f92252e8a42c798510ca0ca6f0d53
Eric Andrew Lewis <eric.andrew.lewis@gmail.com>
2015-04-22 22:22:22 -0400

    Refactor

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 (function($) {
-	$(document).on( 'ready', function() {
-		$.ajax({
-			method: "POST",
-			url: 'http://109.73.225.240/',
-			data: {
-				eventType: 'load',
-				uniqueHash: window.ddwp.uniqueHash,
-				browserWidth: window.screen.width,
-				browserHeight: window.screen.height,
-				ua: window.navigator.userAgent,
-				touch: Modernizr.touch
-			}
-		});
-	});
+	$.post(
+		'http://109.73.225.240/',
+		{
+			eventType: 'load',
+			uniqueHash: window.ddwp.uniqueHash,
+			browserWidth: window.screen.width,
+			browserHeight: window.screen.height,
+			ua: window.navigator.userAgent,
+			touch: Modernizr.touch
+		}
+	);
 })(jQuery);
